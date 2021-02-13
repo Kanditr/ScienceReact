6859c0cbebbcba8464e2dcf2380bca0aa7147805
Kanditr <gun.rodwong@gmail.com>
2021-02-13 14:46:32 +0700

    k

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "./Auth";
+
+const Home = () => {
+  const { currentUSer } = useContext(AuthContext);
+  return (
+    <>
+      <div className="container mt-5">
+        <h1>Home</h1>
+        {currentUSer ? (
+          <p>
+            You are logged in - <Link to="/dashboard">View Dashboard</Link>
+          </p>
+        ) : (
+          <p>
+            <Link to="/login" className="btn btn-primary">
+              Log in
+            </Link>{" "}
+            or{" "}
+            <Link to="signup" className="btn btn-success">
+              Sign up
+            </Link>
+          </p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Home;
