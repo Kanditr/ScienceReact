44bf63c99b9a795d54a3e4a6f2aa6008e0bcc92d
Kanditr <gun.rodwong@gmail.com>
2021-02-13 14:59:21 +0700

    k

---
+++ src/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyAIzbAcCxSifEwYv5tntJPAyxSrvUN5ALs",
+  authDomain: "science3-c85d4.firebaseapp.com",
+  projectId: "science3-c85d4",
+  storageBucket: "science3-c85d4.appspot.com",
+  messagingSenderId: "188042430",
+  appId: "1:188042430:web:55e8db4b564055cb9a62ef",
+  measurementId: "G-Y2XSXP84FE",
+});
+
+export default firebaseConfig;
