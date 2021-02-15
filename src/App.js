897a553c1e7d159f6a8cdef6d4a31f21ba0888ff
Kanditr <gun.rodwong@gmail.com>
2021-02-15 22:29:00 +0700

    k

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 import LogIn from "./components/Login";
-import Signup from "./components/Signup";
+import test from "./components/test";
 import Createproject from "./components/createproject";
 import { AuthProvider } from "./components/Auth";
 
@@ -15,7 +15,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/Dashboard" component={Dashboard} />
           <Route exact path="/login" component={LogIn} />
-          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/signup" component={test} />
           <Route exact path="/createproject" component={Createproject} />
         </Switch>
       </Router>
