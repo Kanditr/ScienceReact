f6bb6d31f0fc9dc6c330d3f69c44a87cae426a08
Kanditr <gun.rodwong@gmail.com>
2021-03-14 00:16:20 +0700

    testSendReceive

--- src/components/TestReceive.js
+++ src/components/TestReceive.js
@@ -1,13 +1,58 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import { Link, useLocation } from "react-router-dom";
 
-export default class TestReceive extends Component {
-  render() {
-    return (
-      <div>
-        <p>Test: {this.props.params.id}</p>
-        <Link to="testsend">back</Link>
-      </div>
-    );
-  }
-}
+const TestReceive = (props) => {
+  // use hook
+  const [title, settitle] = useState();
+  const [body, setbody] = useState();
+  const [id, setid] = useState();
+
+  let Location = useLocation();
+
+  console.log(Location);
+  console.log(props, "props");
+  let Id = Location.value.Id;
+
+  console.log(Id, " Location.data.Id id here new");
+
+  useEffect(() => {
+    axios(` https://jsonplaceholder.typicode.com/posts/${Id} `)
+      .then((data) => {
+        let gettitle = data.data.title;
+        let getbody = data.data.body;
+        let getid = data.data.id;
+
+        settitle(gettitle);
+        setbody(getbody);
+        setid(getid);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Link to="/testsend">
+        <Card.Link>
+          <h1 className="mt-3">Back... </h1>
+        </Card.Link>
+      </Link>
+      <CardGroup>
+        <Card>
+          <Card.Body>
+            <Card.Title className="mt-5">
+              id:{id}. {title}
+            </Card.Title>
+            <Card.Text>{body}</Card.Text>
+          </Card.Body>
+        </Card>
+      </CardGroup>
+    </>
+  );
+};
+
+export default TestReceive;
--- src/components/TestSend.js
+++ src/components/TestSend.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-function TestSend() {
-  let projectid = { id: "acb" };
+export default class TestSend extends Component {
+  state = {
+    post: [],
+  };
 
-  return (
-    <div>
-      <Link to={{ pathname: "/testreceive", state: projectid }}>Test Send</Link>
-    </div>
-  );
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        this.setState({
+          post: res.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <h3 className="mt-3"> Card </h3>
+
+        {this.state.post.map((post) => {
+          let Id = post.id;
+          return (
+            <CardGroup key={post.id} className="m-3">
+              <Card>
+                <Card.Body>
+                  <Card.Title> {post.title} </Card.Title>
+                  <Card.Text>{post.body}</Card.Text>
+                </Card.Body>
+
+                <Card.Footer>
+                  <Link
+                    to={{
+                      pathname: "/testreceive",
+                      value: { Id },
+                    }}
+                  >
+                    <Card.Link>Read More...</Card.Link>
+                  </Link>
+                </Card.Footer>
+              </Card>
+            </CardGroup>
+          );
+        })}
+      </>
+    );
+  }
 }
-
-export default TestSend;
